6383eb89ae9dd56bc075669b63cefd0925a45381
dfkenin <dfkenin@postech.ac.kr>
2023-11-06 04:14:02 +0900

    mod 2-2

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -163,23 +163,32 @@ int read(int fd, void* buffer, unsigned size) {
 int write(int fd, const void* buffer, unsigned size) {
   int num;
   struct thread *cur = thread_current();
+  printf("w1\n");
   validity(buffer);
+  printf("w2\n");
+  printf("fd = %d\n", fd);
   if (fd == 1){
     lock_acquire(&race_lock);
+    printf("w3-1\n");
     putbuf(buffer, size);
     num = size;
   }
   else{
-    if (fd < 1 || fd >= BOUND)
+    if (fd < 0 || fd >= BOUND)
+      printf("w3-2\n");
       exit(-1);
     else {
       struct file *file_ = cur->fd_tab[fd];
+      printf("w3-2\n");
       if (file_ == NULL) exit(-1);
+      printf("w3-4\n");
       lock_acquire(&race_lock);
       num = file_write(file_, buffer, size);
     }
   }
+  printf("w4\n");
   lock_release(&race_lock);
+  printf("w5\n");
   return num;
 }
 
