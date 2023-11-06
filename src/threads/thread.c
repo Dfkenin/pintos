9b10c44bd8e9d328f8945ced73ee475e06c15322
YoungiJang <131528863+YoungiJang@users.noreply.github.com>
2023-11-06 19:56:55 +0900

    mod 3

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -486,6 +486,8 @@ init_thread (struct thread *t, const char *name, int priority)
   sema_init(&(t->load), 0);
   sema_init(&(t->exit), 0);
   #endif
+  //mod 3
+  t->run_file=NULL;
 
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
