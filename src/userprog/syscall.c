9532be88846ab2683744b5f068291bf8408e083f
dfkenin <dfkenin@postech.ac.kr>
2023-12-04 17:31:44 +0900

    proj3

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -126,7 +126,7 @@ syscall_handler (struct intr_frame *f)
   //mod 4 for pt-grow-stk-sc
   thread_current()->esp = f->esp;
   
-  //printf("Syscall num : %d\n", syscall_num);
+  printf("Syscall num : %d\n", syscall_num);
   (syscall_table[syscall_num])(f);
 }
 
