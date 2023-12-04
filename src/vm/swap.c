302a8fce835b079671ebacfbc2b4ff5d677c9b08
dfkenin <dfkenin@postech.ac.kr>
2023-12-04 17:58:49 +0900

    proj3

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -37,6 +37,7 @@ size_t swap_out(void *kpage){
 }
 
 void swap_in(size_t index, void *kpage){
+    printf("swap 0\n");
     lock_acquire(&swap_lock);
     if (bitmap_test(swap_table, index) == 0){
         exit(-1);
@@ -44,10 +45,12 @@ void swap_in(size_t index, void *kpage){
 
     bitmap_set(swap_table, index, 0);
     lock_release(&swap_lock);
+    printf("swap 1\n");
 
     void *buffer = kpage;
     for (int i = 0; i < nsector; ++i){
         block_read(swap_block, index * nsector + i, buffer);
         buffer += BLOCK_SECTOR_SIZE;
     }
+    printf("swap 2\n");
 }
